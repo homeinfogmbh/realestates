a8a4826d5fffba2f449c0232daab665f0bf6e72c
Richard Neumann <r.neumann@homeinfo.de>
2018-07-30 11:06:04 +0200

    Refac.

--- realestates.py
+++ realestates.py
@@ -1,7 +1,5 @@
 """Web application to yield real estates for a respective customer."""
 
-from peewee import DoesNotExist
-
 from mdb import Customer
 from openimmodb import Immobilie
 from wsgilib import Error, HTML, Application
@@ -10,6 +8,7 @@ from wsgilib import Error, HTML, Application
 APPLICATION = Application('realestates', debug=True)
 TITLE = 'Ihre aktuellen Immobilien bei HOMEINFO'
 HEAD = '<head>\n  <title>{}</title>\n</head>'.format(TITLE)
+HEADER = '<h1>Immobilien von "{}" bei HOMEINFO</h1>'
 TEMPLATE = '<!DOCTYPE HTML>\n{}\n{{}}'.format(HEAD)
 COLUMN = '    <td>\n      {}\n    </td>'
 ROW = '  <tr>\n{}\n  </tr>'
@@ -31,16 +30,16 @@ def get(cid):
 
     try:
         customer = Customer.get(Customer.id == cid)
-    except DoesNotExist:
-        raise Error('No such customer.') from None
+    except Customer.DoesNotExist:
+        raise Error('No such customer.')
 
-    header = '<h1>Immobilien von "{}" bei HOMEINFO</h1>'.format(customer)
+    header = HEADER.format(customer)
     rows = [table_header()]
 
     for immobilie in Immobilie.select().where(Immobilie.customer == customer):
-        columns = []
-        columns.append(COLUMN.format(immobilie.objektnr_extern))
-        columns.append(COLUMN.format(immobilie.stand_vom))
+        columns = (
+            COLUMN.format(immobilie.objektnr_extern),
+            COLUMN.format(immobilie.stand_vom))
         rows.append(ROW.format('\n'.join(columns)))
 
     table = TABLE.format('\n'.join(rows))
