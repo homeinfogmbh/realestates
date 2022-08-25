71de9530aed2122b56a6cd49fdc222cfb37070f2
Richard Neumann <r.neumann@homeinfo.de>
2022-08-25 15:54:15 +0200

    Update dependencies
    While at it, migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='realestates',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    python_requires='>=3.8',
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='info@homeinfo.de',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='r.neumann@homeinfo.de',
-    requires=['peewee', 'mdb', 'openimmodb', 'wsgilib',],
+    maintainer_email='<r.neumann@homeinfo.de>',
+    install_requires=[
+        'flask',
+        'mdb',
+        'openimmodb',
+        'wsgilib'
+    ],
     py_modules=['realestates'],
-    description='Real estates API.')
+    description='Real estates API.'
+)
